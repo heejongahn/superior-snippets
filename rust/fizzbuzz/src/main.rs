cd35a56f0d66dc950caf61453433cca5eb9c244d
Heejong Ahn <heejongahn@gmail.com>
2015-07-19 13:59:09 +0900

    Rust: FizzBuzz

---
+++ rust/fizzbuzz/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    for num in 1..100 {
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("FizzBuzz")
+        }
+        else if num % 3 == 0 {
+            println!("Fizz")
+        }
+        else if num % 5 == 0 {
+            println!("Buzz")
+        }
+        else {
+            println!("{}", num)
+        }
+    }
+}
