7771527d55c56dc1fed1a3a39b50a1aaa4c03137
Heejong Ahn <heejongahn@gmail.com>
2015-10-11 20:07:42 +0900

    cpp/ pretty print

---
+++ cpp/pretty_print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()  {
+  cout << setw(7);
+  cout << "abc";
+  cout << setw(7);
+  cout << "def";
+  cout << "haha";
+  cout << setw(7);
+  cout << endl;
+}
